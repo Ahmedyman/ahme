5a6ea3f6acd1cffd3abdfc92b82e274ed8b94e1e
Ahmedyman <91107255+Ahmedyman@users.noreply.github.com>
2024-02-20 02:16:09 +0200

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,14 @@
+
+// function check(){
+//     if(age>30){
+//         return "you are bigger"
+//     }
+//     return "you still young"
+// }
+// let name="ahmed";
+// let age=21;
+// var check= ` i'am ${name}, i have ${age} years old ....i am still ${age>30 ? "young" : "too old"}`
+// console.log(check)
+
+console.log(ger);
+ger=21
