59fe1bb5651e6298d78e987fbaf0f4b9d0c73595
Ahmedyman <91107255+Ahmedyman@users.noreply.github.com>
2024-03-06 20:49:02 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
         navbar.classList.toggle("active");
         overlay.classList.toggle("active");
         document.body.classList.toggle("nav-active");
-    };
+    }
 
     addEventOnElements(navTogglers, "click", toggleNav);
 
@@ -24,40 +24,40 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("scroll", function () {
         header.classList.toggle("active", window.scrollY > 100);
     });
+
+    var addItemId = 0;
+
+    function addToCart(itemId) {
+        addItemId += 1;
+
+        var selectItem = document.createElement('div');
+        selectItem.classList.add('desti-card');
+        selectItem.setAttribute('id', 'cartItem' + addItemId);
+
+        var img = document.createElement('img');
+        img.setAttribute('src', document.getElementById(itemId).querySelector('img').src);
+
+        var title = document.createElement('div');
+        title.innerText = document.getElementById(itemId).querySelector('.title').innerText;
+
+        var label = document.createElement('div');
+        label.innerText = document.getElementById(itemId).querySelector('p').innerText;
+
+        var delbtn = document.createElement('button');
+        delbtn.innerText = 'delete';
+        delbtn.addEventListener('click', function () {
+            del(addItemId);
+        });
+
+        var cartItem = document.getElementById('title');
+        cartItem.append(selectItem);
+        selectItem.append(img);
+        selectItem.append(title);
+        selectItem.append(label);
+        selectItem.append(delbtn);
+    }
+
+    function del(itemId) {
+        document.getElementById('cartItem' + itemId).remove();
+    }
 });
-var addItemId = 0;
-
-function addToCart(itemId){
-    addItemId += 1;
-
-    var selectItem = document.createElement('div');
-    selectItem.classList.add('desti-card');
-    selectItem.setAttribute('id', 'cartItem' + addItemId);
-
-    var img = document.createElement('img');
-    img.setAttribute('src', document.getElementById(itemId).querySelector('img').src);
-
-    var title = document.createElement('div');
-    title.innerText = document.getElementById(itemId).querySelector('.title').innerText;
-
-    var label = document.createElement('div');
-    label.innerText = document.getElementById(itemId).querySelector('p').innerText;
-
-    var delbtn = document.createElement('button');
-    delbtn.innerText = 'delete';
-    delbtn.addEventListener('click', function () {
-        del(addItemId);
-    });
-
-    var cartItem = document.getElementById('title');
-    cartItem.append(selectItem);
-    selectItem.append(img);
-    selectItem.append(title);
-    selectItem.append(label);
-    selectItem.append(delbtn);
-}
-
-function del(itemId) {
-    document.getElementById('cartItem' + itemId).remove();
-}
-
